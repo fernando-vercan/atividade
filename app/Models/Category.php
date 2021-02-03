d76a8cae287edfafabb21c5e35d93a4aa3994e13
fernando.vercan <fsales316@gmail.com>
2021-02-03 10:40:06 -0300

    criação dos controllers

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Category extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'active'];
+
+    public function products (){
+        return $this->belongsToMany(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'price', 'active'];
+
+    public function categories (){
+        return $this->belongsToMany(Category::class);
+    }
 }
