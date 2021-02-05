5ba799d53eb5aeb83da1ed7293205f809feb945f
fernando.vercan <fsales316@gmail.com>
2021-02-05 11:10:44 -0300

    fiz a route de redirecionamento para login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,5 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', function(){
+    return redirect('/login');
+});
 
 require __DIR__.'/auth.php';
